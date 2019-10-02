d962da9e39c5045ebc3bebef65c94cccb4039f3c
saaddalida <dalidassaad@gmail.com>
2019-10-02 17:30:59 -0300

    Setting multer

---
+++ src/controllers/SpotController.js
@@ -0,0 +1,26 @@
+const Spot = require('../models/Spot');
+const User = require('../models/User');
+
+module.exports = {
+  async store(req, res) {
+    const { filename } = req.file;
+    const { company, price, techs } = req.body;
+    const { user_id } = req.headers;
+
+    const user = User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exist' });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      price,
+      techs: techs.split(',').map(tech => tech.trim())
+    });
+
+    return res.json(spot);
+  }
+};
