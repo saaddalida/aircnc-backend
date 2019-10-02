15b1118b19927c8e06b19ecd0de5ce0f39c0ef1d
saaddalida <dalidassaad@gmail.com>
2019-10-02 16:49:09 -0300

    setting up mongodb

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://dbUser:dbPass@cluster0-u7hqu.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
